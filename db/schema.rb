840c2640262377c175f4b3693ebf2fb12f96bc02
maitrey-shroff <maitrey89@me.com>
2017-01-11 19:06:51 -0800

    fixed education model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112024207) do
+ActiveRecord::Schema.define(version: 20170112025419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,21 @@ ActiveRecord::Schema.define(version: 20170112024207) do
     t.string   "screenshot"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "student_id"
   end
 
   create_table "educations", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string   "degree"
+    t.string   "university_name"
+    t.text     "details"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "student_id"
+  end
+
+  create_table "experiences", force: :cascade do |t|
     t.datetime "start_date"
     t.datetime "end_date"
     t.string   "job_title"
@@ -32,12 +44,29 @@ ActiveRecord::Schema.define(version: 20170112024207) do
     t.text     "details"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "student_id"
   end
 
   create_table "skills", force: :cascade do |t|
     t.string   "skill_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.text     "bio"
+    t.string   "linkedin_url"
+    t.string   "twitter_handle"
+    t.string   "personal_url"
+    t.string   "resume_url"
+    t.string   "github_url"
+    t.string   "photo"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
