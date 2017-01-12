d7cf5e446ab77655ea0792a28130db1d0520091e
maitrey-shroff <maitrey89@me.com>
2017-01-11 19:10:39 -0800

    added people-skills join table and created relationships

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
-  # has_many :experiences
-  # has_many :educations
-  # has_many :capstones
+  has_many :experiences
+  has_many :educations
+  has_many :capstones
+  has_many :skills, through: :people_skills
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112025419) do
+ActiveRecord::Schema.define(version: 20170112030802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20170112025419) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "student_id"
+  end
+
+  create_table "people_skills", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "skills", force: :cascade do |t|
