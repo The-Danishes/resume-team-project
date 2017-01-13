0947ff61dc2ea788e2b84eb94630549f1df19c12
maitrey-shroff <maitrey89@me.com>
2017-01-12 18:29:06 -0800

    faker data in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,32 @@ end
 student_list.each do |student|
   Student.create({first_name: student[:first_name],last_name: student[:last_name],email: student[:email],phone_number: student[:phone_number],bio: student[:bio]})
 end
+
+skill_list=[]
+100.times do
+  skill_list.push({
+    skill_name: Faker::Music.instrument
+  })
+end
+
+skill_list.each do |student|
+  Skill.create({skill_name: student[:skill_name]})
+end
+
+capstone_list=[]
+100.times do
+  capstone_list.push({
+    name: Faker::App.name,
+    description: Faker::Hacker.say_something_smart,
+    URL: Faker::Internet.url,
+    screenshot: Faker::Company.logo
+  })
+end
+
+Student.ids.each do |student_id|
+  capstone = capstone_list.sample
+  Capstone.create({name: capstone[:name], description: capstone[:description], URL: capstone[:URL], screenshot: capstone[:screenshot], student_id: student_id})
+  5.times do 
+    PeopleSkill.create({student_id: student_id, skill_id: Skill.ids.sample})
+  end  
+end
